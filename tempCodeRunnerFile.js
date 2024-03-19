aa4280e94f6c22393bad03df50f52accab8afb96
YogaAlfaRisqi <yogariysqi3@gmail.com>
2024-03-19 22:53:44 +0700

    tambah1

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+var a=10;
+var b="xyz";
+var total = a-b;
+console.log(total);
---
+++ variabeltipedata.js
@@ -0,0 +1,25 @@
+//VARIABEL DAN TIPE DATA
+
+// var number = 10;
+// console.log(number);
+
+// var str = "Buku";
+// console.log(str);
+
+// var inCorect = true;
+// console.log(inCorect);
+
+//Operator Aritmatika
+// var a = 10;
+// var b = "xyz";
+// var total = a % b;
+// console.log(total);
+
+//Operator logic
+//== -> membandingkan nilainya saja
+//=== -> membandingkan nilai dan tipe datanya
+
+var x = 10;
+var y = 10;
+var z = x == y;
+console.log(z);
