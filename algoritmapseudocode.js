7d79ef96973ed36958de6a5479edeec345bd61b3
YogaAlfaRisqi <yogariysqi3@gmail.com>
2024-03-19 20:25:38 +0700

    AlgoritmaPseudocode

---
+++ algoritmapseudocode.js
@@ -0,0 +1,47 @@
+//ALGORITMA PSEUDOCODE
+
+/**
+ * Study Case 1
+ * Tentukan Algoritma  dan Pseudocode  menghitung keliling Persegi panjang
+ * Algoritma
+    Buat Variabel panjang
+    Buat Variabel lebar 
+    Buat Variabel keliling = 2 * (panjang + lebar)
+    Cetak Keliling
+ * Pseudocode 
+    isi panjang dengan angka
+    isi lebar dengan angka
+    Isi Keliling = 2 * (Panjang + Lebar)
+    Tampilkan Keliling
+
+ * Study Case 2
+ * Tentukan Algoritma  dan Pseudocode  menghitung luas permukaan tabung
+ * Algoritma
+    Buat Variabel pi
+    Buat Variabel jari-jari alas (r)
+    Buat Variabel tinggi tabung (t)
+    Buat Variabel luasPermukaan
+    Isi luasPermukaan = 2*pi*r(r+t)
+    Cetak Luas Permukaan
+ *Pseudocode
+    Isi variabel pi dengan 3.14
+    Isi jari-jari alas dengan angka
+    Isi tinggi tabung dengan angka
+    Hitung luasPermukaan= 2*pi*r(r+t)
+    Tampilkan luasPermukaan
+
+ * Study Case 3
+ * Tentukan Algoritma  dan Pseudocode  menghitung 1/4 Volume Bola
+ * Algoritma
+   Buat variabel pi
+   Buat Variabel jari-jari bola r
+   Buat variabel volumeBola
+   Isi volumeBola = 1/4*4/3*pi*r^3
+   Cetak Volume
+
+ * PseudoCode
+   isi variabel pi dengan 3.14
+   isi variabel jari-jari r dengan angka
+   isi variabel volumeBola = 1/4*4/3*pi*r^3
+   cetak volumeBola
+ */
